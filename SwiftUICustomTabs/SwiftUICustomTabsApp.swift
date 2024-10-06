1c072bec1e4278d5d9f28780afda0a0dadc9d15a
manuaravind <manuaravindpta@gmail.com>
2024-10-06 20:52:30 +0530

    Initial Commit

---
+++ SwiftUICustomTabs/SwiftUICustomTabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICustomTabsApp.swift
+//  SwiftUICustomTabs
+//
+//  Created by Manu Aravind on 06/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICustomTabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
